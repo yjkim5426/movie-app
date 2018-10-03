cfe71f457de0e4f56d7c459ed8be306483daea49
YJ-toolbox <youngjin@toolbox.net.nz>
2018-10-04 10:56:55 +1300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+// Movie list component
+class App extends Component {
+  // All component must have a render function
+  // It shows what it is.
+  render() {
+    return (
+      <div className="App">
+        
+      </div>
+    );
+  }
+}
+
+export default App;
