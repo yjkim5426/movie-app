e6f6a9a8d039727a75f8ef9ed74a8f7fe5894ac4
YoungJin Kim <yjkim5426@gmail.com>
2018-10-06 21:22:40 +1300

    move movies array in the App component

--- src/App.js
+++ src/App.js
@@ -2,38 +2,47 @@ import React, { Component } from 'react';
 import './App.css';
 import Movie from './Movie';
 
-const movies = [
-  {
-    title: "Matrix",
-    poster: "https://upload.wikimedia.org/wikipedia/en/thumb/c/c1/The_Matrix_Poster.jpg/220px-The_Matrix_Poster.jpg"
-  },
-  {
-    title: "Full metal Jacket",
-    poster: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRCR7dxscYcXcZzqNRSz7KvdmdDJq3rVJKxOzxxLOLJmSg1uRZ_lQ"
-  },
-  {
-    title: "Oldboy",
-    poster: "https://upload.wikimedia.org/wikipedia/en/thumb/6/67/Oldboykoreanposter.jpg/220px-Oldboykoreanposter.jpg"
-  },
-  {
-    title: "Star Wars",
-    poster: "https://images-na.ssl-images-amazon.com/images/I/81mhcR03dIL._SY445_.jpg"
-  }
-];
-
 // Movie list component
 class App extends Component {
 
   state = {
-    greeting: 'Hello!'
+    greeting: 'Hello!',
+    movies: [
+      {
+        title: "Matrix",
+        poster: "https://upload.wikimedia.org/wikipedia/en/thumb/c/c1/The_Matrix_Poster.jpg/220px-The_Matrix_Poster.jpg"
+      },
+      {
+        title: "Full metal Jacket",
+        poster: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRCR7dxscYcXcZzqNRSz7KvdmdDJq3rVJKxOzxxLOLJmSg1uRZ_lQ"
+      },
+      {
+        title: "Oldboy",
+        poster: "https://upload.wikimedia.org/wikipedia/en/thumb/6/67/Oldboykoreanposter.jpg/220px-Oldboykoreanposter.jpg"
+      },
+      {
+        title: "Star Wars",
+        poster: "https://images-na.ssl-images-amazon.com/images/I/81mhcR03dIL._SY445_.jpg"
+      }
+    ]
   }
 
   componentDidMount(){
+    // setTimeout(function(){
+    //   console.log('hello')
+    // }, 2000)
     setTimeout(() => {
       this.setState({
-        greeting: 'Hello again!'
+        movies: [
+          ...this.state.movies,
+          {
+            title: "trainspotting",
+            poster: "https://upload.wikimedia.org/wikipedia/en/thumb/7/71/Trainspotting_ver2.jpg/250px-Trainspotting_ver2.jpg"
+          }
+        ]
       })
-    }, 2000)
+    }, 5000)
+
   }
 
   // All component must have a render function
@@ -41,14 +50,14 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        {this.state.greeting}
+        {/* {this.state.greeting} */}
         {/* Use title props to pass the data to Movie component*/}
         {/* <Movie title={movieTitles[0]} poster={movieImages[0]}/>
         <Movie title={movieTitles[1]} poster={movieImages[1]}/>
         <Movie title={movieTitles[2]} poster={movieImages[2]}/>
         <Movie title={movieTitles[3]} poster={movieImages[3]}/> */}
 
-        {movies.map(movie => {
+        {this.state.movies.map(movie => {
           return <Movie title={movie.title} poster={movie.poster} />
         })}
 
