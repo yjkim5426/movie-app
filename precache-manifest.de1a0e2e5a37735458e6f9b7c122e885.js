4ff221d560c6ffacff84453e90e554cf4eccae7b
YoungJin Kim <yjkim5426@gmail.com>
2018-10-10 23:42:32 +1300

    Updates

---
+++ precache-manifest.de1a0e2e5a37735458e6f9b7c122e885.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2a4a27000ba76122af42",
+    "url": "/movie-app/static/js/runtime~main.2a4a2700.js"
+  },
+  {
+    "revision": "3e64df6c96a74fa4cd2e",
+    "url": "/movie-app/static/js/main.3e64df6c.chunk.js"
+  },
+  {
+    "revision": "07ee25ffef5a954c4c64",
+    "url": "/movie-app/static/js/1.07ee25ff.chunk.js"
+  },
+  {
+    "revision": "3e64df6c96a74fa4cd2e",
+    "url": "/movie-app/static/css/main.20729af6.chunk.css"
+  },
+  {
+    "revision": "75f88dc9d5e9955b721d6154815b12f3",
+    "url": "/movie-app/index.html"
+  }
+];
