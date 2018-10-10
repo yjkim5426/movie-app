3250918e35fbcf9ed2468eb64ffaa8d69d50dc92
YoungJin Kim <yjkim5426@gmail.com>
2018-10-10 23:45:56 +1300

    finish

--- src/App.js
+++ src/App.js
@@ -2,93 +2,51 @@ import React, { Component } from 'react';
 import './App.css';
 import Movie from './Movie';
 
-// Movie list component
 class App extends Component {
 
-  state = {
-  }
+  state = {}
 
   componentDidMount(){
-    fetch('https://yts.ag/api/v2/list_movies.json?sort_by=rating')
-    .then(response => response.json())
-    .then(json => console.log(json))
-    .catch(err => console.log(err))
-    // .catch(function(err){
-    //   console.log(err)
-    // })
-
-    // setTimeout(function(){
-    //   console.log('hello')
-    // }, 2000)
-
-    // setTimeout(() => {
-    //   this.setState({
-    //     movies: [
-    //       {
-    //         title: "Matrix",
-    //         poster: "https://upload.wikimedia.org/wikipedia/en/thumb/c/c1/The_Matrix_Poster.jpg/220px-The_Matrix_Poster.jpg"
-    //       },
-    //       {
-    //         title: "Full metal Jacket",
-    //         poster: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRCR7dxscYcXcZzqNRSz7KvdmdDJq3rVJKxOzxxLOLJmSg1uRZ_lQ"
-    //       },
-    //       {
-    //         title: "Oldboy",
-    //         poster: "https://upload.wikimedia.org/wikipedia/en/thumb/6/67/Oldboykoreanposter.jpg/220px-Oldboykoreanposter.jpg"
-    //       },
-    //       {
-    //         title: "Star Wars",
-    //         poster: "https://images-na.ssl-images-amazon.com/images/I/81mhcR03dIL._SY445_.jpg"
-    //       },
-    //       {
-    //         title: "trainspotting",
-    //         poster: "https://upload.wikimedia.org/wikipedia/en/thumb/7/71/Trainspotting_ver2.jpg/250px-Trainspotting_ver2.jpg"
-    //       }
-    //     ]
-    //   })
-    // }, 2000)
+    this._getMovies();
   }
 
   _renderMovies = () => {
-    const movies = this.state.movies.map(movie => {
-      return <Movie title={movie.title} poster={movie.poster} />
+    const movies = this.state.movies.map((movie) => {
+      return <Movie 
+        title={movie.title_english} 
+        poster={movie.medium_cover_image} 
+        key={movie.id} 
+        genres={movie.genres} 
+        synopsis={movie.synopsis}/>
     })
     return movies
   }
 
-  // All component must have a render function
-  // It shows what it is.
+  _getMovies = async () => {
+    const movies = await this._callApi()
+    this.setState({
+      movies
+    })
+  }
+
+  _callApi = () => {
+    var proxyUrl = 'https://cors-anywhere.herokuapp.com/',
+    targetUrl = 'https://yts.ag/api/v2/list_movies.json?sort_by=download_count'
+
+    return fetch(proxyUrl + targetUrl)
+    .then(potato => potato.json())
+    .then(json => json.data.movies)
+    .catch(err => console.log(err))
+  }
+
   render() {
+    const { movies } = this.state;
     return (
-      <div className="App">
-        {/* {this.state.greeting} */}
-        {/* Use title props to pass the data to Movie component*/}
-        {/* <Movie title={movieTitles[0]} poster={movieImages[0]}/>
-        <Movie title={movieTitles[1]} poster={movieImages[1]}/>
-        <Movie title={movieTitles[2]} poster={movieImages[2]}/>
-        <Movie title={movieTitles[3]} poster={movieImages[3]}/> */}
-
-        {/* {this.state.movies.map(movie => {
-          return <Movie title={movie.title} poster={movie.poster} />
-        })} */}
-
-        {/* Loading */}
-
+      <div className={movies ? "App" : "App--loading"}>
         {this.state.movies ? this._renderMovies() : 'Loading'}
-
-        {/* {[
-          <Movie title={movies[0].title} poster={movies[0].poster} />
-          <Movie title={movies[1].title} poster={movies[1].poster} />
-          <Movie title={movies[2].title} poster={movies[2].poster} />
-          <Movie title={movies[3].title} poster={movies[3].poster} />
-        ]} */}
       </div>
     );
   }
 }
 
 export default App;
-
-
-
-// js file --> react-creact-app --> html
--- src/Movie.js
+++ src/Movie.js
@@ -2,6 +2,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import "./Movie.css";
+import LinesEllipsis from "react-lines-ellipsis";
 
 // class Movie extends Component{
 //     static propTypes = {
@@ -20,11 +21,29 @@ import "./Movie.css";
 //     }
 // }
 
-function Movie({ title, poster }) {
+function Movie({ title, poster, genres, synopsis }) {
   return (
-    <div>
-      <MoviePoster poster={poster} />
-      <h1>{title}</h1>
+    <div className="Movie">
+      <div className="Movie__Columns">
+        <MoviePoster poster={poster} alt={title} />
+      </div>
+      <div className="Movie__Columns">
+        <h1>{title}</h1>
+        <div className="Movie__Genres">
+          {genres.map(genre => (
+            <MovieGenre genre={genre} />
+          ))}
+        </div>
+        <p className="Movie__Synopsis">
+          <LinesEllipsis
+            text={synopsis}
+            maxLine="3"
+            ellipsis="..."
+            trimRight
+            basedOn="letters"
+          />
+        </p>
+      </div>
     </div>
   );
 }
@@ -37,17 +56,28 @@ function Movie({ title, poster }) {
 //     }
 // }
 
-function MoviePoster({ poster }) {
-  return <img src={poster} alt="Movie Poster" />;
+function MoviePoster({ poster, alt }) {
+  return <img src={poster} alt={alt} title={alt} className="Movie__Poster" />;
+}
+
+function MovieGenre({ genre }) {
+  return <span className="Movie_genre">{genre}</span>;
 }
 
 Movie.propTypes = {
-    title:PropTypes.string.isRequired,
-    poster: PropTypes.string.isRequired
-}
+  title: PropTypes.string.isRequired,
+  poster: PropTypes.string.isRequired,
+  genres: PropTypes.string.isRequired,
+  synopsis: PropTypes.string.isRequired,
+  alt: PropTypes.string.isRequired
+};
 
 MoviePoster.propTypes = {
   poster: PropTypes.string.isRequired
+};
+
+MovieGenre.poropType = {
+  genre: PropTypes.string.isRequired
 };
 
 export default Movie;
